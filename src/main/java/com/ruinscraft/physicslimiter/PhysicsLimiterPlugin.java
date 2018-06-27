39443e144096f92bf5ec9bad7de8a784c6415821
Dexton Anderson <contact@dextonanderson.com>
2018-06-27 17:18:14 -0500

    add limit message, increase default physics limit

--- src/main/java/com/ruinscraft/physicslimiter/PhysicsLimiterPlugin.java
+++ src/main/java/com/ruinscraft/physicslimiter/PhysicsLimiterPlugin.java
@@ -2,7 +2,9 @@ package com.ruinscraft.physicslimiter;
 
 import java.util.*;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
@@ -11,6 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class PhysicsLimiterPlugin extends JavaPlugin implements Listener {
 
 	private static Map<Chunk, Integer> chunkUpdates;
+	private static Map<UUID, Long> notifiedAt;
 
 	private long interval;
 	private long physicsLimit;
@@ -20,6 +23,8 @@ public class PhysicsLimiterPlugin extends JavaPlugin implements Listener {
 		saveDefaultConfig();
 		
 		chunkUpdates = new HashMap<>();
+		notifiedAt = new HashMap<>();
+		
 		interval = getConfig().getLong("interval");
 		physicsLimit = getConfig().getLong("physics-limit");
 	
@@ -37,6 +42,17 @@ public class PhysicsLimiterPlugin extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onPhysics(BlockPhysicsEvent event) {
 		if (incrementChunk(event.getBlock().getChunk()) > physicsLimit) {
+			Chunk chunk = event.getBlock().getChunk();
+			
+			for (Entity entity : chunk.getEntities()) {
+				try {
+					if (System.currentTimeMillis() - notifiedAt.getOrDefault(entity.getUniqueId(), 0L) > 120000L) {
+						notifiedAt.put(entity.getUniqueId(), System.currentTimeMillis());
+						entity.sendMessage(ChatColor.RED + "A machine in your chunk has been disabled because it exceeds the max physics updates allowed for this chunk. If you believe this should not be happening, please contact an admin.");
+					}
+				} catch (Exception e) {}
+			}
+			
 			event.setCancelled(true);
 		}
 	}
